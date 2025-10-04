0fa1ed5224359837e124c410c5ce0f492240b8f6
Balázs <halmaibalazss@gmail.com>
2025-10-04 12:05:10 +0200

    Updating person.py with getters and setters

--- Person/main.py
+++ Person/main.py
@@ -1,17 +1,44 @@
-from person import *
+from logging import exception
+
+from person import Person
 
 def main():
     print("Hello World")
-    p1 = Person("p1", "Antal", 45, False)
+    example_name = "Pelda"
+
+    people = []
+    while len(Person.example_names) != 0:
+        example_name = Person.example_names.pop()
+        pe = Person("Id",example_name,27,False)
+        people.append(pe)
+
+    print(people)
+    #if len(Person.example_names) != 0:
+    #    example_name = Person.example_names.pop(0)
+
+
+    p1 = Person("p1", example_name, 45, False)
     p2 = Person("p2", "Barbara", 20, True)
     p3 = Person("p1", "Csaba", 26, True)
 
-    print(p1)
 
-    print(p1 == p2)
-    print(p1 == p3)
-    print(p1.__hash__())
+   #print(p1)
+   #print(p2.id)
 
-    print(p1 > p2)
+   #print(Person.is_adult(p1))
+   #print(Person.num_of_people)
+
+   #print(p1.getId())
+   #print(p1.getName())
+
+    try:
+        p1.age = 16
+    except ValueError:
+        print("Invalid age")
+    except Exception:
+        print("Invalid type")
+
+    print(p1.name)
+
 if __name__ == '__main__':
     main()
--- Person/person.py
+++ Person/person.py
@@ -2,22 +2,59 @@ from functools import total_ordering
 
 @total_ordering
 class Person:
-    id: str
-    name: str
-    age: int
-    smoking: bool
+    __id: str
+    _name: str
+    _age: int
+    _smoking: bool
+
+    AGE_LIMIT = 18
+
+    num_of_people = 0
+
+    example_names = ["Attila","Beáta","Cintia"]
 
     def __init__(self, id, name, age, smoking):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.smoking = smoking
+        self.__id = id
+        self._name = name
+        self._age = age
+        self._smoking = smoking
+
+        Person.num_of_people += 1
+
+    @property
+    def id(self) -> str:
+        return self.__id
+
+    @id.setter
+    def id(self, id: str):
+        self.__id = id
+
+    @property
+    def name(self) -> str:
+        return self._name
+
+    @name.setter
+    def name(self, givenName: str):
+        self._name = givenName
+
+    @property
+    def age(self) -> int:
+        return self._age
+
+    @age.setter
+    def age(self, age: int):
+        if type(age) != int:
+            raise TypeError("age must be an integer")
+
+        if age <= 0 or age > 120:
+            raise ValueError("age must be between 0 and 120")
+
 
     def __str__(self):
-        return '[id = {0}] : name = {1}, age = {2}, smoking = {3}'.format(self.id, self.name, self.age, self.smoking)
+        return '[id = {0}] : name = {1}, age = {2}, smoking = {3}'.format(self.__id, self._name, self._age, self._smoking)
 
     def __repr__(self):
-        return "{id = {0} name = {1}, age = {2}, smoking = {3}}".format(self.id, self.name, self.age, self.smoking)
+        return "id = {0} name = {1}, age = {2}, smoking = {3}".format(self.__id, self._name, self._age, self._smoking)
 
     def __eq__(self, other):
         return self.id == other.id
@@ -30,3 +67,7 @@ class Person:
 
     def __hash__(self):
         return hash(self.id)
+
+    @staticmethod
+    def is_adult(self):
+        return self.age >= Person.AGE_LIMIT
