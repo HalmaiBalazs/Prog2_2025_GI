fe8fb080101ad484ce235df3790acabec75f5a74
Balázs <halmaibalazss@gmail.com>
2025-10-04 10:12:48 +0200

    Separating person.py from main.py

--- Person/main.py
+++ Person/main.py
@@ -1,35 +1,4 @@
-from ftplib import parse227
-
-
-class Person:
-    id: str
-    name: str
-    age: int
-    smoking: bool
-
-    def __init__(self, id, name, age, smoking):
-        self.id = id
-        self.name = name
-        self.age = age
-        self.smoking = smoking
-
-    def __str__(self):
-        return '[id = {0}] : name = {1}, age = {2}, smoking = {3}'.format(self.id, self.name, self.age, self.smoking)
-
-    def __repr__(self):
-        return "{id = {0} name = {1}, age = {2}, smoking = {3}}".format(self.id, self.name, self.age, self.smoking)
-
-    def __eq__(self, other):
-        return self.id == other.id
-
-    def __lt__(self, other):
-        return self.id < other.id
-
-    def __le__(self, other):
-        return self.id <= other.id
-
-    def __hash__(self):
-        return hash(self.id)
+from person import *
 
 def main():
     print("Hello World")
